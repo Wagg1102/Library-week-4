127f5ad1aa680aa61cd601f605714ae6c6fe36ce
huhaowen <Hhwagg1102@outlook.com>
2024-09-01 16:39:38 +1000

    assignment2

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,103 +1,103 @@
 <template>
-    <!-- 🗄️ W3. Library Registration Form -->
-    <div class="container mt-5">
-      <div class="row">
-        <div class="col-md-8 offset-md-2">
-          <h1 class="text-center">🗄️ W5. Library Please login</h1>
+  <!-- 🗄️ W3. Library Registration Form -->
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">🗄️ W5. Library Please login</h1>
 
-          <form @submit.prevent="submitForm">
-            <div class="row mb-3 offset-md-4">
-              <div class="col-md-6 col-sm-6">
-                <label for="username" class="form-label">Username</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="username"
-                  @blur="() => validateName(true)"
-                  @input="() => validateName(false)"
-                  v-model="formData.username"
-                />
-                <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
-              </div>
-           
-  
+        <form @submit.prevent="submitForm">
+          <div class="row mb-3 offset-md-4">
+            <div class="col-md-6 col-sm-6">
+              <label for="username" class="form-label">Username</label>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
+                v-model="formData.username"
+              />
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
-            <div class="row mb-3 offset-md-4">
-              <div class="col-md-6 col-sm-6">
-                <label for="password" class="form-label">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="password"
-                  @blur="() => validatePassword(true)"
-                  @input="() => validatePassword(false)"
-                  v-model="formData.password"
-                />
-                <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
-              </div>
-                   
-              </div>
-            
-            <div class="text-center" style="margin-bottom: 500px;">
-              <button type="submit" class="btn btn-primary me-2">Submit</button>
-              <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
+         
+
+          </div>
+          <div class="row mb-3 offset-md-4">
+            <div class="col-md-6 col-sm-6">
+              <label for="password" class="form-label">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
+                v-model="formData.password"
+              />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
-          </form>
-        </div>
+                 
+            </div>
+          
+          <div class="text-center" style="margin-bottom: 500px;">
+            <button type="submit" class="btn btn-primary me-2">Submit</button>
+            <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
+          </div>
+        </form>
       </div>
     </div>
-  </template>
-  <script setup>
-  import { ref } from 'vue'
-  import router from '../router/index';
-  import { useAuthticate } from '../router/authenticate';
- 
-  const hardCodeUserName = 'user'
-  const hardCodePassword = '12345678'
-  const { isAuthenticated } = useAuthticate()
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import router from '../router/index';
+import { useAuthticate } from '../router/authenticate';
 
-  
-  const formData = ref({
-    username: '',
-    password: ''
+const hardCodeUserName = 'user'
+const hardCodePassword = '12345678'
+const { isAuthenticated } = useAuthticate()
 
-  })
-  
-  
-  const submitForm = () => {
-    validateName(true)
-    validatePassword(true)
-    if (!errors.value.username && !errors.value.password && formData.value.username ===  hardCodeUserName && formData.value.password === hardCodePassword) {
-    alert("login success")
-    isAuthenticated.value = true
-    console.log("loginview",isAuthenticated.value)
-    router.push({name:'About'})
-    }
-  }
-  
-  const errors = ref({
-    username: null,
-    password: null
-  })
-  
-  const validateName = (blur) => {
-    if (formData.value.username.length < 3) {
-      if (blur) errors.value.username = 'Name must be at least 3 characters'
-    } else {
-      errors.value.username = null
-    }
-  }
-  
-  const validatePassword = (blur) => {
- 
 
-  
-    if (formData.value.password.length < 8) {
-      if (blur) errors.value.password = `Password must be at least 8 characters long.`
-    } else {
-      errors.value.password = null
-    }
+const formData = ref({
+  username: '',
+  password: ''
+
+})
+
+
+const submitForm = () => {
+  validateName(true)
+  validatePassword(true)
+  if (!errors.value.username && !errors.value.password && formData.value.username ===  hardCodeUserName && formData.value.password === hardCodePassword) {
+  alert("login success")
+  isAuthenticated.value = true
+  console.log("loginview",isAuthenticated.value)
+  router.push({name:'About'})
   }
-  
-  
-  </script>
+}
+
+const errors = ref({
+  username: null,
+  password: null
+})
+
+const validateName = (blur) => {
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
+}
+
+const validatePassword = (blur) => {
+
+
+
+  if (formData.value.password.length < 8) {
+    if (blur) errors.value.password = `Password must be at least 8 characters long.`
+  } else {
+    errors.value.password = null
+  }
+}
+
+
+</script>
